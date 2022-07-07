9dba582ed006f4c857e20b91c72c591f12c3ed7f
Danielxcott <changwilliam826@gmail.com>
2022-07-07 10:01:08 +0630

    deleted some files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,11 +19,11 @@ use Illuminate\Support\Facades\Route;
 //     return view('master');
 // });
 
-Route::get('/',[PostController::class,'index'])->name('post.index');
-Route::get('/post-create',[PostController::class,'postCreate'])->name('post.create');
-Route::post('/post-create',[PostController::class,'store'])->name('post.store');
-Route::get('/show/{id}',[PostController::class,'show'])->name('post.show');
-Route::delete('/destroy/{id}',[PostController::class,'destroy'])->name('post.destroy'); //use delete method for deleting specific data from database and aslo have to use form and put delete method in there!!
-Route::get('/edit/{id}',[PostController::class,'edit'])->name('post.edit');
-Route::put('/update/{id}',[PostController::class,'update'])->name('post.update');
-//use put method for updating specific data from database and aslo have to use form and 'put' method in there!!
+// Route::get('/',[PostController::class,'index'])->name('post.index');
+// Route::get('/post-create',[PostController::class,'postCreate'])->name('post.create');
+// Route::post('/post-create',[PostController::class,'store'])->name('post.store');
+// Route::get('/show/{id}',[PostController::class,'show'])->name('post.show');
+// Route::delete('/destroy/{id}',[PostController::class,'destroy'])->name('post.destroy'); //use delete method for deleting specific data from database and aslo have to use form and put delete method in there!!
+// Route::get('/edit/{id}',[PostController::class,'edit'])->name('post.edit');
+// Route::put('/update/{id}',[PostController::class,'update'])->name('post.update');
+// //use put method for updating specific data from database and aslo have to use form and 'put' method in there!!
